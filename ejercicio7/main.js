960bff228c88785a41ebc52866717bd7fe73ef01
zerialfan <zerialfan@gmail.com>
2023-12-14 22:25:23 -0600

    add excercise

---
+++ ejercicio7/main.js
@@ -0,0 +1,21 @@
+let array =[]
+array.push(40,50,60)
+array.push(10,20,30)
+
+//Primero teneremos que ordenar la lista 
+let newArray = array.sort((a,b)=> a-b)
+
+// El ejercicio requiere que obtenga la mediana de las listas
+// ojo cuando 
+function isArrayPair(newArray) {
+    const indexList = newArray.length/2
+
+    if (newArray.length % 2 == 0) { 
+        let mid = newArray.length/2 
+        let mediana = ((newArray[mid-1] + newArray[mid])/2)
+        return mediana
+    }else {
+        return  newArray[Math.floor(indexList)]
+    }
+}
+console.log(newArray,isArrayPair(newArray));
